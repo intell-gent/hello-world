ae374dee54d90f10fb74d4c819a4238c94bd56d0
intell-gent <nick.kaser@yahoo.com>
2016-07-10 21:26:54 -0700

    Delete scoops.js

--- Head FIrst/scoops.js
+++
@@ -1,7 +0,0 @@
-var scoops = 5;
-while (scoops > 0) {
-	document.write("Another scoop!<br>");
-	scoops = scoops -1;
-	console.log(scoops);
-}
-document.write("Life without ice cream isn't the same");
