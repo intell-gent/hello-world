13d99ccd7527eb1d84c5b4c232e41cecebc34ff2
intell-gent <nick.kaser@yahoo.com>
2016-07-10 21:22:17 -0700

    Delete code.js

--- Head FIrst/code.js
+++
@@ -1,6 +0,0 @@
-var scoop = document.getElementById("rasberry");
-var altText = scoop.getAttribute("alt");
-console.log("I can't see the image in the console,");
-console.log(" but i'm told it looks like: " + altText);
-
-
